1c9d2555588b1059ece46a30472798ea4dbfb0cd
Роман Николаевич <rnkalinin@yandex.ru>
2022-03-22 20:57:14 +0300

    Added image and button with toast

--- app/src/main/java/com/rays95/cinemarays/MainActivity.kt
+++ app/src/main/java/com/rays95/cinemarays/MainActivity.kt
@@ -2,10 +2,16 @@ package com.rays95.cinemarays
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_1.setOnClickListener {
+            Toast.makeText(this, "Vrum-Vrum!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
