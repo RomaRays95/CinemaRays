3559b977ee1b7d9d13099c09026c14213e5b386c
Роман Николаевич <rnkalinin@yandex.ru>
2022-03-23 01:35:25 +0300

    menu with buttons added

--- app/src/main/java/com/rays95/cinemarays/MainActivity.kt
+++ app/src/main/java/com/rays95/cinemarays/MainActivity.kt
@@ -11,7 +11,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         button_1.setOnClickListener {
-            Toast.makeText(this, "Vrum-Vrum!", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Task 1", Toast.LENGTH_SHORT).show()
+        }
+        button_2.setOnClickListener {
+            Toast.makeText(this, "Task 2", Toast.LENGTH_SHORT).show()
+        }
+        button_3.setOnClickListener {
+            Toast.makeText(this, "Task 3", Toast.LENGTH_SHORT).show()
+        }
+        button_4.setOnClickListener {
+            Toast.makeText(this, "Task 4", Toast.LENGTH_SHORT).show()
+        }
+        button_5.setOnClickListener {
+            Toast.makeText(this, "Task 5", Toast.LENGTH_SHORT).show()
         }
     }
 }
